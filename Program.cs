ca33fca20304ed50bf338fbb850192b8a62d7efc
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2025-09-28 14:33:26 -0400

    Instalando pacotes necessários

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Rota padrão");
+
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+  if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "12345678")
+    return Results.Ok("Login realizado com sucesso");
+  else
+    return Results.Unauthorized();
+});
 
 app.Run();
+
+public class LoginDTO
+{
+  // `default!;` diz que a propriedade não é nullable
+  public string Email { get; set; } = default!;
+  public string Senha { get; set; } = default!;
+}
