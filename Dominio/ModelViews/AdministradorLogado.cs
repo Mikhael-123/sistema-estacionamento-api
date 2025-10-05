26fc8baf1fab2244241b167650a4c1ac6c85e6f5
Mikhael-123 <paulomiguelbentes@gmail.com>
2025-10-04 22:00:21 -0400

    Refatorando

--- Dominio/ModelViews/AdministradorLogado.cs
+++
@@ -1,8 +0,0 @@
-namespace MinimalApi.Dominio.ModelViews;
-
-public record AdministradorLogado
-{
-  public string Email { get; set; } = default!;
-  public string Perfil { get; set; } = default!;
-  public string Token { get; set; } = default!;
-}
--- Dominio/ModelViews/Home.cs
+++
@@ -1,7 +0,0 @@
-namespace MinimalApi.Dominio.ModelViews;
-
-public struct Home
-{
-  public string Mensagem { get => "API de veículos"; }
-  public string Docs { get => "/swagger"; }
-}
